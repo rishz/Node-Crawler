e3432e8045664e9f08a2e8101e5ffbc95f29c2b5
Rishabh Shukla <rishbabhshukla25@hotmail.com>
2019-04-06 14:21:38 +0530

    Fix: Scrape method to count URLs

--- controllers/scrapeController.js
+++ controllers/scrapeController.js
@@ -2,16 +2,24 @@ const request = require('request-promise');
 const cheerio = require('cheerio');
 
 exports.scrape = url => {
-	console.log(url);
 	return new Promise((resolve, reject) => {
 		request(url)
 		    .then(html => {
 		    	$ = cheerio.load(html);
-			    links = $('a'); // jQuery get all hyperlinks
+			    links = $('a'); // jQuery to get all hyperlinks
+			    let urls = {};
+
 			    $(links).each(function(i, link){
 			    	console.log($(link).text() + ':\n  ' + $(link).attr('href'));
+			    	let url = $(link).attr('href');
+			    	if(url in urls){
+			    		urls[url] += 1;
+			    	}else{
+			    		urls[url] = 1;
+			    	}
 			  	});
-		        resolve();
+
+		        resolve(urls);
 		    })
 		    .catch(err => {
 		        reject(err);
